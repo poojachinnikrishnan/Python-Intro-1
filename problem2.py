5b5a700ee9f09c0bb1f936b08dd07a4be45aa7b2
poojachinnikrishnan <60472985+poojachinnikrishnan@users.noreply.github.com>
2020-02-07 23:07:33 +0530

    Solutions for problem1 and problem2

---
+++ problem2.py
@@ -0,0 +1,7 @@
+s="HumptyDumptysatonawallHumptyDumptyhadagreatfallAlltheKingshorsesandalltheKingsmenCouldntputHumptyDumptyinhisplaceagain."
+a=22
+b=27
+c=97
+d=102
+output=s[a:b+1]+" "+s[c:d+1]
+print (output)
